2c8a5e3f4a69f443cabded9c5b46ee198e5a56e0
Bill Nguyen <billfl93@gmail.com>
2022-10-11 19:56:04 -0400

    commit tree 1d31a8661be72b897b7db6396695d3068f173cc9

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
+import {useEffect, useState} from "react"
 // create your App component here
+
+function App() {
+    const [image, setImage] = useState(null)
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+          .then((response) => response.json())
+          .then((data) => {
+            setImage(data.message);
+          });
+      }, []);
+
+      if (!image) return <p>Loading...</p>
+
+    return <img src={image} alt="A Random Dog" />  
+        
+    
+}
+
+export default App
